c10e2fd2c6b938e551c1796d188f669bbb2e9e10
SurpriseBox <latyshevilia@gmail.com>
2019-12-01 15:59:19 +0300

    body onresize func added, so page's behavior on different resolutions changed

--- sources/styles/scripts/js_scripts.js
+++ sources/styles/scripts/js_scripts.js
@@ -12,7 +12,7 @@ function scroll_to_bottom(){
 function scroll_to_top(){
 	var to_top = document.body.scrollTop;
 	if(to_top > 0){
-		window.scrollBy(0, (to_top+100)/-10);
+		window.scrollBy(0, (to_top+100)/-20);
 		timeout = setTimeout('scroll_to_top()', 20);
 	}
 	else clearTimeout(timeout);
@@ -21,6 +21,21 @@ function scroll_to_top(){
 function change_viewport(){
 	if(window.innerWidth < 1173){
 		document.body.style.cssText += 'overflow-x: visible; margin-left: auto; margin-right: auto;';
-	};
+	}
+	else{
+		document.body.style.cssText += 'overflow-x: hidden; margin-left: calc((100% - 1600px)/2); margin-right: calc((100% - 1600px)/2);';
+	}
 }
 
+
+function scale_content(){
+	var scale = window.innerWidth/1600;
+	if(scale < 0.8) scale = 0.8;
+	if(scale > 2.4) scale = 2.4;
+	document.body.style.cssText += 'transform: scale(' + scale + ');';
+}
+
+function body_onresize(){
+	change_viewport();
+	/*scale_content();*/
+}
